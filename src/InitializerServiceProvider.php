4933f8b2c104d0c4ee981167e94a5062bc82d1a1
Artem Stepanenko <stpnnk.artem@gmail.com>
2024-03-13 14:48:52 +0200

    dev-l11-compatibility

--- src/InitializerServiceProvider.php
+++ src/InitializerServiceProvider.php
@@ -3,19 +3,10 @@
 namespace Stepanenko3\LaravelInitializer;
 
 use Illuminate\Support\ServiceProvider;
-use NunoMaduro\LaravelConsoleTask\LaravelConsoleTaskServiceProvider;
 use Stepanenko3\LaravelInitializer\Contracts\Runner;
 
 class InitializerServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        $this->app->register(LaravelConsoleTaskServiceProvider::class);
-    }
-
     /**
      * Register the application services.
      */
